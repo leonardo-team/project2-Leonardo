551054a3e19ea3e8daee73c0a90311a2731b5340
maxy23 <max_0474@mail.ru>
2021-10-29 20:30:35 +0600

    fixed a few places

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { adminLoginForm } from './view/elements/AdminLoginForm';
-import { errorPage } from './view/pages/ErrorPage';
+//  import { adminLoginForm } from './view/elements/AdminLoginForm';
+// import { errorPage } from './view/pages/ErrorPage';
+import { MoreDetails } from './view/pages/MoreDetails/MoreDetails';
 
 function App() {
   return (
     <div className="App">
-   
+   <MoreDetails/>
     </div>
   );
 }
