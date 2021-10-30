9ab6324f029d35cc9a65dbbbbeb1e1d257cac3cb
Kirilll <vilyam13@gmail.com>
2021-10-30 18:39:26 +0300

    Add eventsMutations.ts

--- client/src/view/pages/Admin.tsx
+++ client/src/view/pages/Admin.tsx
@@ -1,5 +1,3 @@
-export function admin() {
-    return (
-        <div className="admin">Здесь будет ваша реклама. Компонент admin</div>
-    )
+export function Admin() {
+  return <div className="admin">Здесь будет ваша реклама. Компонент admin</div>;
 }
