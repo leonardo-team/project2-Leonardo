26306eafc07783e01ed8f6ed7e3569a652df99e3
popetrov <popetrov64@gmail.com>
2021-11-05 13:31:18 +0400

    merge conflict

--- client/src/view/pages/Control.tsx
+++ client/src/view/pages/Control.tsx
@@ -3,13 +3,6 @@ import { Header } from '../components/Header';
 
 export function control() {
   return (
-<<<<<<< HEAD
     <div className="control">Здесь будет ваша реклама. Компонент control</div>
-=======
-        <div className="control">
-            <Header />
-            <Footer />
-        </div>
->>>>>>> 2bc39d0 (admin no functional)
   );
 }
