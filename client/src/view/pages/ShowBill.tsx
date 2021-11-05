c293c160f8f197d14f96d84b0a842d118423f334
popetrov <popetrov64@gmail.com>
2021-11-05 13:34:28 +0400

    conflict showBill

--- client/src/view/pages/ShowBill.tsx
+++ client/src/view/pages/ShowBill.tsx
@@ -3,13 +3,6 @@ import { Header } from '../components/Header';
 
 export function showBill() {
   return (
-<<<<<<< HEAD
     <div className="showBill">Здесь будет ваша реклама. Компонент showBill</div>
-=======
-        <div className="showBill">
-            <Header />
-            <Footer />
-        </div>
->>>>>>> 2bc39d0 (admin no functional)
   );
 }
