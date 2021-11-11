11235b5ae1d107e7086287476091fd85027a4106
pvpavlova <91315362+pvpavlova@users.noreply.github.com>
2021-11-11 11:23:12 +0500

    trouble

--- sale_site/src/components/registration/registration.tsx
+++ sale_site/src/components/registration/registration.tsx
@@ -1,25 +1,71 @@
+import { FC, useCallback, useState } from 'react';
 import '../css/site.css'
 
+const EMPTY_FORM = {
+  login: '',
+  password: '',
+  password2: '',  
+};
 
-export function Registration(){
+export const RegisterForm: FC = () => {
+  const [form, setForm] = useState(EMPTY_FORM);
+
+  const formClearHandle = useCallback((e) => {
+    e.preventDefault();
+
+    setForm(EMPTY_FORM);
+  }, []);
+
+  const inputChangeHandle = useCallback((e) => {
+    const { target } = e;
+
+    setForm(prev => ({
+      ...prev,
+      [target.name]: target.value,
+    }));
+  }, []);
+
+  const formSubmitHandle = useCallback((e) => {
+    e.preventDefault();
+
+    console.log(form);
+  }, [form]);
+
   return (
-    <div className="Registration">
-        <div className="Login_page">
-
-        </div>
-        <div className="Registration Login_page">
-            <label htmlFor="">Логин</label>
-            <input className="Login_page-login" type="text" />
-            <label htmlFor="">Придумайте пароль</label>
-            <input className="Login_page-password" type="password" />
-            <label htmlFor="">Введите пароль еще раз</label>
-            <input className="Login_page-password" type="password" />
-        </div>
-        <div className="button">
-            <button className="mdl-button mdl-js-button mdl-button--raised">
-                Зарегистрироваться
-            </button>
-        </div>
-    </div>
-  )
-}
+    <form className="RegisterForm" onSubmit={formSubmitHandle}>
+        <div className="RegisterForm-Field">
+        <label htmlFor="login">Имя пользователя</label>
+        <input
+          className="RegisterForm-Control"
+          id="login"
+          name="login"
+          onChange={inputChangeHandle}
+          value={form.login}
+        />
+      </div>
+      <div className="RegisterForm-Field">
+        <label htmlFor="password">Пароль</label>
+        <input
+          className="RegisterForm-Control"
+          id="password"
+          name="password"
+          type="password"
+          onChange={inputChangeHandle}
+          value={form.password}
+        />
+      </div>    
+      <div className="RegisterForm-Field">
+        <label htmlFor="comment">Повторите пароль</label>
+        <textarea
+          className="RegisterForm-Control"
+          id="password2"
+          name="password2"
+          onChange={inputChangeHandle}
+          value={form.password2}
+        />
+      </div>
+      <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary" type="submit" onClick={formSubmitHandle}>Отправить</button>
+      <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" type="button" onClick={formClearHandle}>Очистить</button>
+    </form>
+  );
+};
