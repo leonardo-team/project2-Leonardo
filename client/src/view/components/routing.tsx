4c4765549b5edf66ae6e0fad300de4f8232c4b09
Kirilll <vilyam13@gmail.com>
2021-11-12 09:41:32 +0300

    Add redux and types

--- client/src/view/components/routing.tsx
+++ client/src/view/components/routing.tsx
@@ -1,41 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router,
-  Switch,
-  Route,
-  Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Admin } from '../pages/Admin';
 import { ShowBill } from '../pages/ShowBill';
 import { Tickets } from '../pages/Tickets';
-
-const routes = [
-  {
-    path: '/',
-    exact: true,
-    sidebar: () =><Admin path={'/'}/>
-  },
-  {
-    path: '/ShowBill.tsx',
-    sidebar: () => <ShowBill path={'/ShowBill.tsx'}/>
-  },
-  {
-    path: '/Tickets.tsx',
-    sidebar: () => <Tickets path={'/Tickets.tsx'}/>
-  }
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { actions } from '../../store/actions';
+import { selectStatistic, selectEventList } from '../../store/selectors/selectors';
+import { useQuery } from '@apollo/client';
+import { STATISTIC_QUERY } from '../../queries/statisticQuery';
 
 export default function Routing() {
+  const dispatch = useDispatch();
+
+  const { loading, data = {} } = useQuery(STATISTIC_QUERY);
+  const { statistics = [] } = data;
+  if (statistics.length > 0) {
+    dispatch(actions.getStatistic(statistics[0]));
+  }
+
+  const routes = [
+    {
+      path: '/',
+      exact: true,
+      sidebar: () => (
+        <Admin
+          path={'/'}
+          statistic={useSelector(selectStatistic)}
+          eventData={useSelector(selectEventList)}
+        />
+      ),
+    },
+    {
+      path: '/ShowBill.tsx',
+      exact: false,
+      sidebar: () => <ShowBill path={'/ShowBill.tsx'} />,
+    },
+    {
+      path: '/Tickets.tsx',
+      exact: false,
+      sidebar: () => <Tickets path={'/Tickets.tsx'} />,
+    },
+  ];
+
   return (
-      <Router>
-            <Switch>
-              {routes.map((route, index) => (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  children={<route.sidebar />}
-                />
-              ))}
-            </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} exact={route.exact} children={<route.sidebar />} />
+        ))}
+      </Switch>
+    </Router>
   );
 }
