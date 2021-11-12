8db91cd1228bf3ef692e46d8bc92b84d0485b572
Kirilll <vilyam13@gmail.com>
2021-11-12 09:38:56 +0300

    Add props to Admin.tsx

--- client/src/view/pages/Admin.tsx
+++ client/src/view/pages/Admin.tsx
@@ -4,22 +4,23 @@ import { Statistic } from '../components/AdminComponents/Statistic';
 import { Footer } from '../components/Footer';
 import { Header } from '../components/Header';
 import { SideMenu } from '../components/SideMenu';
+import { IAdminState } from '../../types';
 
 import { TableEvents } from '../components/AdminComponents/TableEvent';
 import { FC } from 'react';
 
-export type AdminProps = {
-  path?: string,
-};
-
-export const Admin: FC<AdminProps> = ({ path }) => {
+export const Admin: FC<IAdminState> = ({ path, statistic, eventData }) => {
   return (
     <div className="admin">
       <SideMenu path={path} />
       <main>
         <Header path={path} />
-        <Statistic />
-        <TableEvents />
+        <Statistic
+          planned={statistic.planned}
+          completed={statistic.completed}
+          canceled={statistic.canceled}
+        />
+        <TableEvents eventData={eventData} />
       </main>
       <Footer />
     </div>
