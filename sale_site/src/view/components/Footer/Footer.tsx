1fe9dbb11b426fbbe2449c0337ff726d50479c83
Kirilll <vilyam13@gmail.com>
2021-11-13 21:10:09 +0300

    Update Admin@

---
+++ sale_site/src/view/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import '../css/site.css'
+
+export const Footer = ()=>{
+
+  return (
+
+            <footer className="mdl-mini-footer">
+                <div className="mdl-mini-footer__left-section">
+                  <div className="mdl-logo">
+                    &copy; "Leonardo"
+                  </div>
+                </div>
+            </footer>
+      
+  );
+}
