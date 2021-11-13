4524368690637be62bf9eabbfc7956a78bd4c8c8
popetrov <popetrov64@gmail.com>
2021-11-13 15:58:35 +0400

    resolve

--- sale_site/src/view/page/HomePage.tsx
+++ sale_site/src/view/page/HomePage.tsx
@@ -1,7 +1,6 @@
 import '../components/css/site.css';
 import { FC } from 'react';
 import { Footer } from '../components/Footer/Footer';
-
 import { Header } from '../components/Header/Header';
 import { Home } from '../components/Home/Home';
 import { SiteAppState } from '../../types';
